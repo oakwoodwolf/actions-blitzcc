fb4984534edcde8a874a4e3ab0165bcaf13a3ff0
Roy Flaherty <royfla100@gmail.com>
2026-04-06 16:10:11 +0100

    Resolve decls from consuming repository

--- scripts/setup.ps1
+++ scripts/setup.ps1
@@ -10,4 +10,19 @@ if (-not (Test-Path "$compilerPath\bin\blitzcc.exe")) {
     throw "Blitz3D compiler not found at: $compilerPath\bin\blitzcc.exe"
 }
 
+Write-Host "Copying userlibraries into the Blitz3D path..."
+Get-ChildItem -Path $env:GITHUB_WORKSPACE -Filter *.dll -File |
+    Copy-Item -Destination "$compilerPath\bin" -Force
+    Copy-Item -Destination "$compilerPath\userlibs" -Force
+
+$declFiles = Get-ChildItem -Path $env:GITHUB_WORKSPACE -Filter *.decl -File
+
+if ($declFiles.Count -eq 0) {
+    Write-Host "::warning::No .decl files found in repository root. Does your project include any Blitz3D user libraries?"
+}
+else {
+    Write-Host "Copying .decl files to Blitz3D userlibs..."
+    $declFiles | Copy-Item -Destination "$compilerPath\userlibs" -Force
+}
+
 Write-Output "compiler_path=$($compilerPath)" >> $Env:GITHUB_OUTPUT
