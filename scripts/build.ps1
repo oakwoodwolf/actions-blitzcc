4b96e970e2f8de8c1b516e6083a6699d5b201891
Roy Flaherty <12115201+oakwoodwolf@users.noreply.github.com>
2026-04-07 18:23:26 +0100

    Remove SWDX exclusive code

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -2,40 +2,9 @@ param(
     [string]$source,
     [string]$output,
     [string]$name,
-    [string]$bump_version,
     [string]$compiler_path
 )
 $env:blitzpath = $compiler_path
-
-if ($bump_version -ne "") {
-    Write-Host "Bumping version in $source..."
-    $content = Get-Content $source -Raw
-
-    $pattern = "Global\s+$bump_version\$=`"(\d+)\.(\d+)\.(\d+)\.Beta(\d+)`""
-    Write-Host "Looking for pattern: $pattern"
-    if ($content -match $pattern) {
-
-        $major = [int]$matches[1]
-        $minor = [int]$matches[2]
-        $patch = [int]$matches[3]
-        $beta  = [int]$matches[4] + 1
-
-        $newVersion = "$major.$minor.$patch.Beta$beta"
-
-        $newLine = "Global $bump_version$=`"$newVersion`""
-
-        $content = [regex]::Replace($content, $pattern, $newLine)
-
-        Set-Content $source $content
-
-        Write-Host "Version bumped to $newVersion"
-
-        $env:GAME_VERSION = $newVersion
-    }
-    else {
-        throw "$bump_version not found."
-    }
-}
 
 Write-Host "Compiling $source"
 
@@ -72,18 +41,3 @@ if (-not (Test-Path "$name.exe")) {
 Copy-Item "$name.exe" $output -Force
 Write-Host "Executable created at: $output"
 Write-Host "Build completed successfully."
-
-
-if ($bump_version -ne "") {
-    git config user.name "github-actions"
-    git config user.email "actions@github.com"
-
-    git add $source
-    git commit -m "Auto bump version to $env:GAME_VERSION"
-
-    git tag "$env:GAME_VERSION"
-
-    git push
-    git push origin "$env:GAME_VERSION"
-    echo "GAME_VERSION=$env:GAME_VERSION" >> $env:GITHUB_ENV
-}
